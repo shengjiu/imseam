6f28803493f0e88852370be16810b9d4129967a4
shengjiu <shengjiu@gmail.com>
2013-12-23 14:28:15 -0700

    Update IJedisCommand.java

--- Raptor/src/com/imseam/raptor/cluster/redis/jedis/cache/IJedisCommand.java
+++ Raptor/src/com/imseam/raptor/cluster/redis/jedis/cache/IJedisCommand.java
@@ -3,10 +3,10 @@ package com.imseam.raptor.cluster.redis.jedis.cache;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Transaction;
 
-public interface IJedisCommand<T, W>{
+public interface IJedisCommand<T>{
 
 	T doImmediate(Jedis jedis);
 	
-	Response<W> doInTransaction(Transaction transaction);
+	void doInTransaction(Transaction transaction);
 	
 }
